d504e11ad2a6778a9a3c92a769e169a2ca929896
mzyl <maxzylstra@gmail.com>
2020-11-30 17:46:04 -0600

    skip loading fixed

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ def preconfig():
 def postconfig():
   cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
   cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
+  cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128 
+  cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1 
   return Visualizer(img[:, :, ::-1], metadata=tree_metadata, scale=1.0)
 
 
